e6aca32034d71aed8d68451a4d2cdabcb565beff
agambini <andrea.gambini@datalogic.com>
2020-02-05 10:37:28 +0100

    Creation and access implementation + tests

--- include/packed_optional/packed_optional.hpp
+++ include/packed_optional/packed_optional.hpp
@@ -1,5 +1,52 @@
-namespace packed_optional {
-    class packed_optional {
+#include <exception>
 
+// TODO:
+// check noexcept
+// add constexpr
+// add rvalue ref overloads
+// assert T constraints
+// add and check specific exception
+
+namespace packed_optional {
+    template<typename T, T empty_value>
+    class packed_optional {
+    public:
+        packed_optional() {
+        }
+
+        packed_optional(T value)
+            : value_{ value } {
+        }
+
+        bool has_value() const noexcept {
+            return value_ != empty_value;
+        }
+
+        explicit operator bool() const noexcept {
+            return has_value();
+        }
+
+        T value() const {
+            if (!has_value()) {
+                throw std::exception();
+            }
+
+            return value_;
+        }
+
+        T value_or(T default_value) const {
+            return has_value() ? value_ : default_value;
+        }
+
+        const T& operator*() const {
+            return value_
+        }
+
+        T& operator*() {
+            return value_;
+        }
+
+    private:
+        T value_ = empty_value;
     };
 }
--- test/packed_optional_test.cpp
+++ test/packed_optional_test.cpp
@@ -4,7 +4,98 @@
 #include "packed_optional/packed_optional.hpp"
 
 namespace po = packed_optional;
+constexpr int g_empty_value{ -1 };
+constexpr int g_default_value{ 42 };
+constexpr int g_value{ 10 };
+using popt = po::packed_optional<int, g_empty_value>;
 
-TEST_CASE("Quick check") {
-    REQUIRE(true);
+template<typename T, T empty_value>
+void require_empty(po::packed_optional<T, empty_value> opt) {
+    REQUIRE_FALSE(opt.has_value());
+    REQUIRE_FALSE(opt);
+
+    REQUIRE_THROWS(opt.value());
+    REQUIRE(opt.value_or(g_default_value) == g_default_value);
+}
+
+template<typename T, T empty_value>
+void require_has_value(po::packed_optional<T, empty_value> opt, T value) {
+    REQUIRE(opt.has_value());
+    REQUIRE(opt);
+
+    REQUIRE(opt.value() == value);
+    REQUIRE(opt.value_or(g_default_value) == value);
+    REQUIRE(*opt == value);
+}
+
+TEST_CASE("Creation") {
+    SUBCASE("Empty") {
+        popt opt;
+
+        require_empty(opt);
+    }
+
+    SUBCASE("From empty value") {
+        popt opt{ g_empty_value };
+
+        require_empty(opt);
+    }
+
+    SUBCASE("From value") {
+        popt opt{ g_value };
+
+        require_has_value(opt, g_value);
+    }
+
+    SUBCASE("From other packed_optional") {
+        SUBCASE("Empty") {
+            popt opt;
+            popt copy{ opt };
+
+            require_empty(copy);
+        }
+
+        SUBCASE("With value") {
+            popt opt{ g_value };
+            popt copy{ opt };
+
+            require_has_value(copy, g_value);
+        }
+    }
+}
+
+TEST_CASE("Copy") {
+    SUBCASE("From other packed_optional") {
+        SUBCASE("Empty") {
+            popt opt;
+            popt copy;
+            copy = opt;
+
+            require_empty(copy);
+        }
+
+        SUBCASE("With value") {
+            popt opt{ g_value };
+            popt copy;
+            copy = opt;
+
+            require_has_value(copy, g_value);
+        }
+    }
+
+    SUBCASE("From T") {
+        SUBCASE("Empty") {
+            popt copy;
+            copy = g_empty_value;
+
+            require_empty(copy);
+        }
+
+        SUBCASE("With value") {
+            popt copy;
+            copy = g_value;
+
+            require_has_value(copy, g_value);
+        }
+    }
 }
