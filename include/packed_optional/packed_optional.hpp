9da9b5608a48b96296eebd541edf9532e10eba53
Andrea Gambini <andrea.gambini@datalogic.com>
2020-02-14 15:14:28 +0100

    Check that T is valid non-type template param

--- include/packed_optional/packed_optional.hpp
+++ include/packed_optional/packed_optional.hpp
@@ -3,13 +3,34 @@
 // TODO:
 // check noexcept
 // add constexpr
-// add rvalue ref overloads (maybe not needed since T must be integral?)
-// assert T constraints
 // add and check specific exception
-// add travis on Win
+
+namespace{
+    template<bool C, typename T1, typename T2>
+    using conditional_t = typename std::conditional<C, T1, T2>::type;
+
+    template<bool C, typename T>
+    using enable_if_t = typename std::enable_if<C, T>::type;
+    
+    template<class...> struct disjunction : std::true_type { };
+    template<class B1> struct disjunction<B1> : B1 { };
+    template<class B1, class... Bn>
+    struct disjunction<B1, Bn...> 
+    : conditional_t<bool(B1::value), B1, disjunction<Bn...>> {};
+}
 
 namespace packed_optional {
-    template<typename T, T empty_value>
+    template<typename T>
+    using is_valid_optional = disjunction<std::is_integral<T>, 
+                                          std::is_pointer<T>, 
+                                          std::is_member_pointer<T>, 
+                                          std::is_enum<T>,
+                                          std::is_same<T, std::nullptr_t>>;
+
+    template<typename T>
+    using valid_optional_t = enable_if_t<is_valid_optional<T>::value,T>;
+                                    
+    template<typename T, valid_optional_t<T> empty_value>
     class packed_optional {
     public:
         packed_optional() = default;
